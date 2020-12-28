20342a697c2f8833687ef643f50162efd3c85873
Jonathan Fritz <fritz@nosc.io>
2020-12-28 13:51:56 +0100

    Adds request model

---
+++ models/Request.ts
@@ -0,0 +1,14 @@
+/**
+ * Represents a registration request
+ */
+export interface Request {
+    itemIds: number[], // items which were requested
+    responsibleUserId: number, // the user responsible for the requested reservation
+    assignedUserId?: number, // this user could be assigned to a request operation
+    startDate: number, // start date of the reservation reqeust
+    plannedEndDate: number // end date of the reservation request
+    note?: string, // notes provided by the user making the request
+    created: number,
+    modified?: number,
+    priority?: number,
+}
