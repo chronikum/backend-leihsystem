c7b9ac376e487e6035fe0c9ff8fcc3403188bab6
Jonathan Fritz <fritz@nosc.io>
2021-02-02 00:08:23 +0100

    Added enums/AccessoireCategory.ts to the project

---
+++ enums/AccessoireCategory.ts
@@ -0,0 +1,9 @@
+/**
+ * TODO: Clarifification how many are available in general.
+ */
+export enum AccessoireCategory {
+    APPLE_PEN_GENERATION_1 = 'APPLE_PEN_GENERATION_1',
+    APPLE_PEN_GENERATION_2 = 'APPLE_PEN_GENERATION_2',
+    IPAD_CASE = 'IPAD_CASE',
+    USB_C_CHARGER = 'USB_C_CHARGER',
+}
